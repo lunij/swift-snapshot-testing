f42fd7c9227d9f32816c628d3bcc31e90ed9b11c
Marc Schultz <lunij@users.noreply.github.com>
2024-06-19 21:34:43 +0200

    Introduce `convertToImage` method

--- Sources/SnapshotTesting/Snapshotting/NSView.swift
+++ Sources/SnapshotTesting/Snapshotting/NSView.swift
@@ -28,17 +28,10 @@
       ).asyncPullback { view in
         let initialSize = view.frame.size
         if let size = size { view.frame.size = size }
-        guard view.frame.width > 0, view.frame.height > 0 else {
-          fatalError("View not renderable to image at size \(view.frame.size)")
-        }
         return view.snapshot
           ?? Async { callback in
             addImagesForRenderedViews(view).sequence().run { views in
-              let bitmapRep = view.bitmapImageRepForCachingDisplay(in: view.bounds)!
-              view.cacheDisplay(in: view.bounds, to: bitmapRep)
-              let image = NSImage(size: view.bounds.size)
-              image.addRepresentation(bitmapRep)
-              callback(image)
+              callback(view.convertToImage())
               views.forEach { $0.removeFromSuperview() }
               view.frame.size = initialSize
             }
