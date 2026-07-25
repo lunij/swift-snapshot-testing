ab5d2f3aa9f64e7df347f0bbcaee40d332b3a3bd
Marc Lambertz <lunij@users.noreply.github.com>
2026-07-25 13:48:24 +0200

    Migrate to `WithSnapshotTestingTests` to Swift Testing

--- Tests/SnapshotTestingTests/WithSnapshotTestingTests.swift
+++ Tests/SnapshotTestingTests/WithSnapshotTestingTests.swift
@@ -1,34 +1,31 @@
-import XCTest
+import Testing
 
 @_spi(Internals) @testable import SnapshotTesting
 
-class WithSnapshotTestingTests: XCTestCase {
-  func testNesting() {
+struct WithSnapshotTestingTests {
+  @Test func nesting() {
     withSnapshotTesting(record: .all) {
-      XCTAssertEqual(
+      #expect(
         SnapshotTestingConfiguration.current?
-          .diffTool?(currentFilePath: "old.png", failedFilePath: "new.png"),
-        """
-        @−
-        "file://old.png"
-        @+
-        "file://new.png"
+          .diffTool?(currentFilePath: "old.png", failedFilePath: "new.png") == """
+            @−
+            "file://old.png"
+            @+
+            "file://new.png"
 
-        To configure output for a custom diff tool, use 'withSnapshotTesting'. For example:
+            To configure output for a custom diff tool, use 'withSnapshotTesting'. For example:
 
-            withSnapshotTesting(diffTool: .ksdiff) {
-              // ...
-            }
-        """
+                withSnapshotTesting(diffTool: .ksdiff) {
+                  // ...
+                }
+            """
       )
-      XCTAssertEqual(SnapshotTestingConfiguration.current?.record, .all)
+      #expect(SnapshotTestingConfiguration.current?.record == .all)
       withSnapshotTesting(diffTool: "ksdiff") {
-        XCTAssertEqual(
-          SnapshotTestingConfiguration.current?
-            .diffTool?(currentFilePath: "old.png", failedFilePath: "new.png"),
-          "ksdiff old.png new.png"
-        )
-        XCTAssertEqual(SnapshotTestingConfiguration.current?.record, .all)
+        let command = SnapshotTestingConfiguration.current?
+          .diffTool?(currentFilePath: "old.png", failedFilePath: "new.png")
+        #expect(command == "ksdiff old.png new.png")
+        #expect(SnapshotTestingConfiguration.current?.record == .all)
       }
     }
   }
