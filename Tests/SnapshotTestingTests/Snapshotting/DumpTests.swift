43fe8a9decaa2bebd832caada6660281b30cda20
Marc Lambertz <lunij@users.noreply.github.com>
2026-07-25 17:24:56 +0200

    Migrate `DumpTests` to Swift Testing

--- Tests/SnapshotTestingTests/Snapshotting/DumpTests.swift
+++ Tests/SnapshotTestingTests/Snapshotting/DumpTests.swift
@@ -1,16 +1,16 @@
 import Foundation
-import XCTest
+import SnapshotTesting
+import Testing
 
-@testable import SnapshotTesting
-
-final class DumpTests: BaseTestCase {
-  func testAny() async {
+@Suite(.snapshots(record: .failed, diffTool: .ksdiff))
+struct DumpTests {
+  @Test func `object dump`() async {
     struct User { let id: Int, name: String, bio: String }
     let user = User(id: 1, name: "Blobby", bio: "Blobbed around the world.")
     await assertSnapshot(of: user, as: .dump)
   }
 
-  func testRecursion() async {
+  @Test func `recursive dump`() async {
     await withSnapshotTesting {
       class Father {
         var child: Child?
@@ -30,32 +30,32 @@ final class DumpTests: BaseTestCase {
     }
   }
 
-  func testAnySnapshotStringConvertible() async {
+  @Test func `StringConvertible dump`() async {
     await assertSnapshot(of: "a" as Character, as: .dump, named: "character")
     await assertSnapshot(of: Data("Hello, world!".utf8), as: .dump, named: "data")
     await assertSnapshot(of: Date(timeIntervalSinceReferenceDate: 0), as: .dump, named: "date")
     await assertSnapshot(of: NSObject(), as: .dump, named: "nsobject")
     await assertSnapshot(of: "Hello, world!", as: .dump, named: "string")
     await assertSnapshot(of: "Hello, world!".dropLast(8), as: .dump, named: "substring")
-    await assertSnapshot(of: URL(string: "https://www.pointfree.co")!, as: .dump, named: "url")
+    await assertSnapshot(of: URL(string: "https://www.apple.com")!, as: .dump, named: "url")
   }
 
-  func testDeterministicDictionaryAndSetSnapshots() async {
+  @Test func `Dictionary and Set dump`() async {
     struct Person: Hashable { let name: String }
     struct DictionarySetContainer { let dict: [String: Int], set: Set<Person> }
     let set = DictionarySetContainer(
       dict: ["c": 3, "a": 1, "b": 2],
-      set: [.init(name: "Brandon"), .init(name: "Stephen")]
+      set: [.init(name: "Bob"), .init(name: "John")]
     )
     await assertSnapshot(of: set, as: .dump)
   }
 
-  func testMultipleSnapshots() async {
+  @Test func `multiple dumps`() async {
     await assertSnapshot(of: [1], as: .dump)
     await assertSnapshot(of: [1, 2], as: .dump)
   }
 
-  func testNamedAssertion() async {
+  @Test func `named dump`() async {
     struct User { let id: Int, name: String, bio: String }
     let user = User(id: 1, name: "Blobby", bio: "Blobbed around the world.")
     await assertSnapshot(of: user, as: .dump, named: "named")
