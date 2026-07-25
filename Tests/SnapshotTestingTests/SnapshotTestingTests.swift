84a0dd863c4804eb55871c4cdb069bb72936a81f
Marc Lambertz <lunij@users.noreply.github.com>
2026-07-25 15:42:05 +0200

    Extract `ImagePrecisionTests`

--- Tests/SnapshotTestingTests/SnapshotTestingTests.swift
+++ Tests/SnapshotTestingTests/SnapshotTestingTests.swift
@@ -21,10 +21,6 @@ import UIKit.UIView
 #endif
 
 final class SnapshotTestingTests: BaseTestCase {
-  private let fixturesURL = URL(fileURLWithPath: #filePath, isDirectory: false)
-    .deletingLastPathComponent()
-    .appendingPathComponent("__Fixtures__", isDirectory: true)
-
   func testAutolayout() async {
     #if os(iOS)
     let vc = UIViewController()
@@ -84,46 +80,6 @@ final class SnapshotTestingTests: BaseTestCase {
     view.layer?.cornerRadius = 5
     await assertSnapshot(of: view, as: .image, named: "\(platform)\(osVersion.majorVersion)")
     await assertSnapshot(of: view, as: .recursiveDescription, named: platform)
-  }
-  #endif
-
-  #if os(iOS) || os(macOS) || os(tvOS)
-  func testPrecision() async {
-    let view = XView(frame: .init(x: 0, y: 0, width: 100, height: 100))  // 10000 pixels
-    view.backgroundColor = .blue
-    await assertSnapshot(of: view, as: .image(precision: 1, perceptualPrecision: 1), named: "\(platform)-original")
-
-    let subview = XView(frame: .init(x: 0, y: 0, width: 10, height: 10))  // 100 pixels
-    subview.backgroundColor = .red
-    view.addSubview(subview)
-    await assertSnapshot(of: view, as: .image(precision: 1, perceptualPrecision: 1), named: "\(platform)-modified")
-
-    var message = await verifySnapshot(of: view, as: .image(precision: 0.999, perceptualPrecision: 1), named: "\(platform)-original", record: .never)
-    let firstLine = message?.split(whereSeparator: \.isNewline).first
-    XCTAssertEqual(firstLine, "[\(platform)-original] Image does not match reference (pixel precision 0.995 is less than required 0.999).")
-
-    // 10000-100=9900 => 99% precision
-    message = await verifySnapshot(of: view, as: .image(precision: 0.99, perceptualPrecision: 1), named: "\(platform)-original", record: .never)
-    XCTAssertNil(message)
-  }
-
-  func testPerceptualPrecision() async {
-    let view = XView(frame: .init(x: 0, y: 0, width: 100, height: 100))
-    view.backgroundColor = .black
-    await assertSnapshot(of: view, as: .image(precision: 1, perceptualPrecision: 1), named: platform + "-original")
-
-    view.backgroundColor = .init(white: 0.0019, alpha: 1)
-    await assertSnapshot(of: view, as: .image(precision: 1, perceptualPrecision: 1), named: platform + "-modified")
-
-    await assertSnapshot(of: view, as: .image(precision: 1, perceptualPrecision: 0.98), named: platform + "-original", record: .never)
-  }
-
-  func testImagePrecision() async throws {
-    let imageURL = fixturesURL.appendingPathComponent("testImagePrecision.reference.png")
-    let image = try XCTUnwrap(XImage(contentsOf: imageURL))
-
-    await assertSnapshot(of: image, as: .image(precision: 0.995), named: "exact")
-    await assertSnapshot(of: image, as: .image(perceptualPrecision: 0.98), named: "perceptual")
   }
   #endif
 
@@ -1273,31 +1229,4 @@ private let allContentSizes =
     "accessibility-extra-extra-large": .accessibilityExtraExtraLarge,
     "accessibility-extra-extra-extra-large": .accessibilityExtraExtraExtraLarge
   ]
-#endif
-
-#if os(iOS) || os(macOS) || os(tvOS)
-extension XImage {
-  convenience init?(contentsOf url: URL) {
-    #if os(iOS) || os(tvOS)
-    self.init(contentsOfFile: url.path)
-    #elseif os(macOS)
-    self.init(byReferencing: url)
-    #endif
-  }
-}
-#endif
-
-#if os(macOS)
-extension XView {
-  var backgroundColor: NSColor? {
-    get {
-      guard let cgColor = layer?.backgroundColor else { return nil }
-      return NSColor(cgColor: cgColor)
-    }
-    set {
-      wantsLayer = true
-      layer?.backgroundColor = newValue?.cgColor
-    }
-  }
-}
 #endif
---
+++ Tests/SnapshotTestingTests/Snapshotting/ImagePrecisionTests.swift
@@ -0,0 +1,83 @@
+#if os(iOS) || os(macOS) || os(tvOS)
+import Foundation
+import XCTest
+
+@testable import SnapshotTesting
+
+#if canImport(AppKit)
+import AppKit
+#endif
+#if canImport(UIKit)
+import UIKit
+#endif
+
+final class ImagePrecisionTests: BaseTestCase {
+  private let fixturesURL = URL(fileURLWithPath: #filePath, isDirectory: false)
+    .deletingLastPathComponent()
+    .deletingLastPathComponent()
+    .appendingPathComponent("__Fixtures__", isDirectory: true)
+
+  func testPrecision() async {
+    let view = XView(frame: .init(x: 0, y: 0, width: 100, height: 100))  // 10000 pixels
+    view.backgroundColor = .blue
+    await assertSnapshot(of: view, as: .image(precision: 1, perceptualPrecision: 1), named: "\(platform)-original")
+
+    let subview = XView(frame: .init(x: 0, y: 0, width: 10, height: 10))  // 100 pixels
+    subview.backgroundColor = .red
+    view.addSubview(subview)
+    await assertSnapshot(of: view, as: .image(precision: 1, perceptualPrecision: 1), named: "\(platform)-modified")
+
+    var message = await verifySnapshot(of: view, as: .image(precision: 0.999, perceptualPrecision: 1), named: "\(platform)-original", record: .never)
+    let firstLine = message?.split(whereSeparator: \.isNewline).first
+    XCTAssertEqual(firstLine, "[\(platform)-original] Image does not match reference (pixel precision 0.995 is less than required 0.999).")
+
+    // 10000-100=9900 => 99% precision
+    message = await verifySnapshot(of: view, as: .image(precision: 0.99, perceptualPrecision: 1), named: "\(platform)-original", record: .never)
+    XCTAssertNil(message)
+  }
+
+  func testPerceptualPrecision() async {
+    let view = XView(frame: .init(x: 0, y: 0, width: 100, height: 100))
+    view.backgroundColor = .black
+    await assertSnapshot(of: view, as: .image(precision: 1, perceptualPrecision: 1), named: platform + "-original")
+
+    view.backgroundColor = .init(white: 0.0019, alpha: 1)
+    await assertSnapshot(of: view, as: .image(precision: 1, perceptualPrecision: 1), named: platform + "-modified")
+
+    await assertSnapshot(of: view, as: .image(precision: 1, perceptualPrecision: 0.98), named: platform + "-original", record: .never)
+  }
+
+  func testImagePrecision() async throws {
+    let imageURL = fixturesURL.appendingPathComponent("testImagePrecision.reference.png")
+    let image = try XCTUnwrap(XImage(contentsOf: imageURL))
+
+    await assertSnapshot(of: image, as: .image(precision: 0.995), named: "exact")
+    await assertSnapshot(of: image, as: .image(perceptualPrecision: 0.98), named: "perceptual")
+  }
+}
+
+extension XImage {
+  convenience init?(contentsOf url: URL) {
+    #if os(iOS) || os(tvOS)
+    self.init(contentsOfFile: url.path)
+    #elseif os(macOS)
+    self.init(byReferencing: url)
+    #endif
+  }
+}
+
+#if os(macOS)
+extension XView {
+  var backgroundColor: NSColor? {
+    get {
+      guard let cgColor = layer?.backgroundColor else { return nil }
+      return NSColor(cgColor: cgColor)
+    }
+    set {
+      wantsLayer = true
+      layer?.backgroundColor = newValue?.cgColor
+    }
+  }
+}
+#endif
+#endif
