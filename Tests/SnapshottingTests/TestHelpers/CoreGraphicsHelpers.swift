c8a35922f0bbef17d58b388ae8c86918391d1e99
Marc Lambertz <lunij@users.noreply.github.com>
2026-07-29 16:58:44 +0200

    Split test helpers and rename containing folder

--- Tests/SnapshottingTests/TestHelpers/CoreGraphicsHelpers.swift
+++ Tests/SnapshottingTests/TestHelpers/CoreGraphicsHelpers.swift
@@ -1,15 +1,3 @@
-import Foundation
-
-let osVersion = ProcessInfo.processInfo.operatingSystemVersion
-
-#if os(iOS)
-let platform = "ios"
-#elseif os(macOS)
-let platform = "macos"
-#elseif os(tvOS)
-let platform = "tvos"
-#endif
-
 #if os(iOS) || os(macOS) || os(tvOS)
 import CoreGraphics
 
