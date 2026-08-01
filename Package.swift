74e5f9c716fdad9a9d3b5ef58f5729336cc5cb13
Marc Lambertz <lunij@users.noreply.github.com>
2026-08-01 22:24:25 +0200

    Raise swift-custom-dump to the first version that builds for tvOS

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
     .library(name: "SnapshottingCustomDump", targets: ["SnapshottingCustomDump"])
   ],
   dependencies: [
-    .package(url: "https://github.com/pointfreeco/swift-custom-dump", from: "1.3.3"),
+    .package(url: "https://github.com/pointfreeco/swift-custom-dump", from: "1.5.0"),
     .package(url: "https://github.com/swiftlang/swift-syntax", "509.0.0"..<"605.0.0"),
     .package(url: "https://github.com/swiftlang/swift-docc-plugin", from: "1.0.0")
   ],
