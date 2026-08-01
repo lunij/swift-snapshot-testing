b4d6b1cce2f6529294928a2839b6289667fb6450
Marc Lambertz <lunij@users.noreply.github.com>
2026-08-01 15:12:10 +0200

    Document the two parameters the iOS path strategy left out

--- Sources/Snapshotting/Strategies/CGPath.swift
+++ Sources/Snapshotting/Strategies/CGPath.swift
@@ -77,6 +77,8 @@ extension SnapshotStrategy where Value == CGPath, Format == UIImage {
   ///     match. 98-99% mimics
   ///     [the precision](http://zschuessler.github.io/DeltaE/learn/#toc-defining-delta-e) of the
   ///     human eye.
+  ///   - scale: The scale to use when loading the reference image from disk.
+  ///   - drawingMode: The drawing mode.
   public static func image(
     precision: Float = 1,
     perceptualPrecision: Float = 1,
